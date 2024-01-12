00026a80750c11f5c2372de4018757d4a102c8fe
Kevin <kevin.rodriguez678@gmail.com>
2024-01-12 14:30:29 -0500

    create handleChange function to save value set by user in the input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [year, setYear] = useState(0)
+
+  function handleChange(e) {
+    setYear(e.target.value)
+    console.log(e.target.value);
+  }
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1>Find Out How Old Your Dog Is In Human Years!</h1>
+        <form>
+          <input 
+            type="number" 
+            placeholder='Enter Dog Year Here' 
+            onChange={handleChange}
+          />
+          <button>Enter</button>
+        </form>
+        
+      </header>
     </div>
   );
 }
